6396e2c284f9e4dac2bd661bf92b830e88188cc3
murilo813 <murilodesouza555@gmail.com>
2025-04-08 09:21:31 -0300

    adição do campo obs para as notas, documentação e reorganização do código

--- routes/addorcamento.py
+++ routes/addorcamento.py
@@ -5,7 +5,6 @@ addorcamento_bp = Blueprint('addorcamento', __name__, template_folder='templates
 @addorcamento_bp.route('/addorcamento', methods=['GET', 'POST'])
 def add_orcamento():
     if request.method == 'POST':
-        # Coleta os dados do formulário
         produtor = request.form['produtor']
         validade = request.form['validade']
         data = request.form['data']
@@ -14,12 +13,8 @@ def add_orcamento():
                        request.form.getlist('quantidade[]'), 
                        request.form.getlist('preco[]'))
 
-        # Salve os dados no banco de dados aqui (exemplo com SQLAlchemy ou outra abordagem)
-        # Seu código para salvar os dados vai aqui
 
-        # Após salvar, redireciona para a página de gerenciamento
-        return redirect(url_for('gerencia.gerencia'))  # Ajuste para a rota correta
+        return redirect(url_for('gerencia.gerencia'))  
 
-    # Caso seja um GET, renderiza o formulário para novo orçamento
     return render_template('addorcamento.html')
 
